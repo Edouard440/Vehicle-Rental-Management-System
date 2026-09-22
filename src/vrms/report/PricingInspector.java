d3d6cbff471e42629729e14edd6c33d99ca5d789
13PoPaul <paul.iglesias--tallon@efrei.net>
2026-09-21 23:41:34 -0400

    Libéréééééééééé délivréééééééééééé je nee mentiraiss plus jamaissssssss

--- src/vrms/report/PricingInspector.java
+++ src/vrms/report/PricingInspector.java
@@ -3,16 +3,16 @@ package vrms.report;
 import vrms.annotation.PricingOption;
 import vrms.contract.PricingPolicy;
 
-public class PricingInspector{
-    public static void inspectPricingPolicy(Class<? extends PricingPolicy> policyClass){
-        System.out.println("Pricing Policy :");
-
-        if (policyClass.isAnnotationPresent(PricingOption.class)){
+public class PricingInspector {
+    public static void inspectPricingPolicy(Class<? extends PricingPolicy> policyClass) {
+        System.out.println("----------------------------------------");
+        if (policyClass.isAnnotationPresent(PricingOption.class)) {
             PricingOption annotation = policyClass.getAnnotation(PricingOption.class);
-            System.out.println("Policy: "+annotation.name());
-            System.out.println("Class: "+policyClass.getSimpleName());
-        }else{
-            System.out.println("No Pricing option annotation found");
+            System.out.println("Pricing Policy:");
+            System.out.println("  Policy : " + annotation.name());
+            System.out.println("  Class  : " + policyClass.getSimpleName());
+        } else {
+            System.out.println("No Pricing option annotation found for " + policyClass.getSimpleName());
         }
     }
 }
